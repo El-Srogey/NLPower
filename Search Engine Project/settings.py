4e28b327f95e39a49059122532aa56a2197062c6
M. El-SROGEY <123467925+El-Srogey@users.noreply.github.com>
2023-05-15 19:12:47 +0300

    Update settings.py

--- Search Engine Project/settings.py
+++ Search Engine Project/settings.py
@@ -1,8 +1,8 @@
 # To Setup OUR PROJECT
 # Made VARs CAPTIAL, TO MAKE IT TO EASY TO BE DISTINGUISHED FROM OUTTER VARs
 
-SEARCH_KEY = 'AIzaSyDlLv0jjJynQwKgQ9_CFVrM-gRaSb87vN0'  # API-Key
-SEARCH_ID = 'd6feeae5672ee49ef' # Search Engine Id
+SEARCH_KEY = ''  # API-Key
+SEARCH_ID = '' # Search Engine Id
 
 COUNTRY = 'eg'  # Egypt Two Letter Coutry Code
 SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl=" + COUNTRY
